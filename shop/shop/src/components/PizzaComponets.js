0f4da9fd64694003918420a5f1067ab0589072c5
junghyunsoo24 <117528532+junghyunsoo24@users.noreply.github.com>
2023-06-10 20:19:44 +0900

    Update PizzaComponets.js db x

--- shop/shop/src/components/PizzaComponets.js
+++ shop/shop/src/components/PizzaComponets.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import "../App.css"
+import "../App.css";
 
 function PizzaComponent() {
   useEffect(() => {
@@ -37,6 +37,9 @@ function PizzaComponent() {
           "allMenuItems",
           "allPagination"
         );
+        // addSortingButton(); // '모두' 탭을 클릭했을 때 정렬 버튼 추가
+        // addIncreaseSortingButton();
+        // addDecreaseSortingButton();
         break;
       case "craftsmanship":
         menuItems = db.getCraftsmanshipMenuItems();
@@ -46,6 +49,10 @@ function PizzaComponent() {
           "craftsmanshipMenuItems",
           "craftsmanshipPagination"
         );
+        removeSortingButton(); // '모두' 탭이 아닌 경우 정렬 버튼 제거
+        removeIncreaseSortingButton();
+        removeDecreaseSortingButton();
+
         break;
       case "expert":
         menuItems = db.getExpertMenuItems();
@@ -55,6 +62,9 @@ function PizzaComponent() {
           "expertMenuItems",
           "expertPagination"
         );
+        removeSortingButton(); // '모두' 탭이 아닌 경우 정렬 버튼 제거
+        removeIncreaseSortingButton();
+        removeDecreaseSortingButton();
         break;
       case "luxury":
         menuItems = db.getLuxuryMenuItems();
@@ -64,9 +74,12 @@ function PizzaComponent() {
           "luxuryMenuItems",
           "luxuryPagination"
         );
+        removeSortingButton(); // '모두' 탭이 아닌 경우 정렬 버튼 제거
+        removeIncreaseSortingButton();
+        removeDecreaseSortingButton();
         break;
     }
-    addPizzaImageEvent(); // 피자 사진에 이벤트 추가
+    addPizzaImageEvent();
   }
 
   function setupPagination(
@@ -109,30 +122,153 @@ function PizzaComponent() {
       var menuItem = currentPageItems[i];
 
       var menuItemDiv = document.createElement("div");
-      menuItemDiv.className = "menu-item";
+      // menuItemDiv.className = "menu-item-image";
 
       var menuItemImage = document.createElement("img");
       menuItemImage.src = menuItem.imageSrc;
       menuItemImage.className = "menu-item-image";
+      menuItemDiv.appendChild(menuItemImage);
 
       var menuItemName = document.createElement("name");
       menuItemName.innerHTML = menuItem.name;
       menuItemName.className = "menu-item-name";
-
-      // var menuItemDescription = document.createElement("p");
-      // menuItemDescription.innerHTML = menuItem.description;
-      // menuItemDescription.className = "menu-item-description";
+      menuItemDiv.appendChild(menuItemName);
 
       var menuItemPrice = document.createElement("price");
       menuItemPrice.innerHTML =
-        "L: " + menuItem.price + "원R: " + (menuItem.price - 4500) + "원";
-
-      menuItemDiv.appendChild(menuItemImage);
-      menuItemDiv.appendChild(menuItemName);
-      // menuItemDiv.appendChild(menuItemDescription);
+        "L: " + menuItem.price + "원 R: " + (menuItem.price - 4500) + "원";
       menuItemDiv.appendChild(menuItemPrice);
 
       menuContainer.appendChild(menuItemDiv);
+    }
+    addPizzaImageEvent();
+    addSortingButton(); // '모두' 탭을 클릭했을 때 정렬 버튼 추가
+    addIncreaseSortingButton();
+    addDecreaseSortingButton();
+  }
+
+  function addPizzaImageEvent() {
+    var pizzaImages = document.getElementsByClassName("menu-item-image");
+    for (let i = 0; i < pizzaImages.length; i++) {
+      var reviewButton = document.createElement("button");
+      var detailButton = document.createElement("button");
+  
+      pizzaImages[i].addEventListener("mouseover", function () {
+        this.style.transform = "scale(1.1)"; // 이미지 확대
+        reviewButton.innerHTML = "리뷰보기 " + i; // 페이지 번호 증가
+        reviewButton.className = "review-button";
+        reviewButton.style.position = "absolute";
+        reviewButton.style.top = "60px";
+        reviewButton.style.left = "100px";
+        reviewButton.style.zIndex = "1";
+        this.parentNode.style.position = "relative";
+        this.parentNode.appendChild(reviewButton);
+  
+        detailButton.innerHTML = "상세보기";
+        detailButton.className = "detail-button";
+        detailButton.style.position = "absolute";
+        detailButton.style.top = "100px";
+        detailButton.style.left = "100px";
+        detailButton.style.zIndex = "1";
+        this.parentNode.style.position = "relative";
+        this.parentNode.appendChild(detailButton);
+        // alert("You clicked on a pizza image!");
+      });
+  
+      pizzaImages[i].addEventListener("mouseout", function () {
+        this.style.transform = "scale(1)"; // 이미지 축소
+        // reviewButton.remove();
+      });
+    }
+  }
+  
+  
+
+  function addSortingButton() {
+    var sortingButton = document.createElement("sortingButton");
+    sortingButton.innerHTML = "정렬";
+    // sortingButton.addEventListener("click", toggleSorting);
+    document.getElementById("allMenuItems").appendChild(sortingButton);
+  }
+
+  function removeSortingButton() {
+    var sortingButton = document.getElementById("sortingButton");
+    if (sortingButton) {
+      sortingButton.remove();
+    }
+  }
+
+  function addIncreaseSortingButton() {
+    var increaseSortingButton = document.createElement("button");
+    increaseSortingButton.innerHTML = "가격높은순";
+    increaseSortingButton.addEventListener("click", function () {
+      var menuContainer = document.getElementById("allMenuItems");
+      var paginationContainer = document.getElementById("allPagination");
+      var menuItems = db.getAllMenuItems();
+
+      // 가격이 높은 순으로 정렬
+      menuItems.sort(function (a, b) {
+        // 가격이 같을 경우 가나다 순으로 정렬
+        if (b.price === a.price) {
+          return a.name.localeCompare(b.name);
+        }
+        return b.price - a.price;
+      });
+
+      setupPagination(menuItems, itemsPerPage, "allMenuItems", "allPagination");
+      showMenuItems(menuItems, 1, itemsPerPage, menuContainer);
+
+      // // 다시 이벤트 추가
+      // addPizzaImageEvent();
+      // addSortingButton();
+      // addIncreaseSortingButton();
+      // addDecreaseSortingButton();
+    });
+
+    document.getElementById("allMenuItems").appendChild(increaseSortingButton);
+  }
+
+  function removeIncreaseSortingButton() {
+    var sortingButton = document.getElementById("sortingButton");
+    if (sortingButton) {
+      sortingButton.remove();
+    }
+  }
+
+  function addDecreaseSortingButton() {
+    var sortingButton = document.createElement("button");
+    sortingButton.innerHTML = "가격낮은순";
+    sortingButton.addEventListener("click", function () {
+      var menuContainer = document.getElementById("allMenuItems");
+      var paginationContainer = document.getElementById("allPagination");
+      var menuItems = db.getAllMenuItems();
+
+      // 가격이 낮은 순으로 정렬
+      menuItems.sort(function (a, b) {
+        // 가격이 같을 경우 가나다 순으로 정렬
+        if (a.price === b.price) {
+          return a.name.localeCompare(b.name);
+        }
+        return a.price - b.price;
+      });
+
+      setupPagination(menuItems, itemsPerPage, "allMenuItems", "allPagination");
+      showMenuItems(menuItems, 1, itemsPerPage, menuContainer);
+
+      // // 다시 이벤트 추가
+      // addPizzaImageEvent();
+      // addSortingButton();
+      // addIncreaseSortingButton();
+      // addDecreaseSortingButton();
+    });
+
+    document.getElementById("allMenuItems").appendChild(sortingButton);
+  }
+
+  function removeDecreaseSortingButton() {
+    var sortingButton = document.getElementById("sortingButton");
+    if (sortingButton) {
+      sortingButton.remove();
     }
   }
 
@@ -279,15 +415,6 @@ function PizzaComponent() {
       ];
     },
   };
-
-  function addPizzaImageEvent() {
-    var pizzaImages = document.getElementsByClassName("menu-item-image");
-    for (var i = 0; i < pizzaImages.length; i++) {
-      pizzaImages[i].addEventListener("click", function () {
-        alert("You clicked on a pizza image!");
-      });
-    }
-  }
 
   return (
     <div className="App">
